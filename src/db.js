985d0795a866bca626ddabd5b3b1038e4a74d106
Sarawut0234 <AppleN0234@gmail.com>
2026-02-26 23:28:54 +0700

    update files

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "test",
+  database: process.env.DB_NAME || "railway",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
